59edfe0e70f88b76780db36edd490138817b9f99
belke05 <you@example.com>
2019-10-15 10:47:03 +0200

    blank template

--- server/app.js
+++ server/app.js
@@ -52,9 +52,7 @@ app.use(
 )
 require('./passport')(app)
 
-app.use('/api', require('./routes/index'))
 app.use('/api', require('./routes/auth'))
-app.use('/api/countries', require('./routes/countries'))
 
 // For any routes that starts with "/api", catch 404 and forward to error handler
 app.use('/api/*', (req, res, next) => {
